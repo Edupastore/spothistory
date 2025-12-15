f0c99141181f053e0bcda59c4f7b8f5224eed575
Eduardo Pastor Ruiz <121805331+Edupastore@users.noreply.github.com>
2025-12-15 21:30:42 +0100

    Update spotify_auto_history.py
    Actualización del archivo .py para recabar más datos.

--- spotify_auto_history.py
+++ spotify_auto_history.py
@@ -31,15 +31,42 @@ data = sp.current_user_recently_played(limit=50)
 items = data.get("items", [])
 
 rows = []
+
 for item in items:
     track = item["track"]
+    album = track["album"]
+    artist = track["artists"][0]
+
+    # --------- Llamadas extra a la API ---------
+    artist_full = sp.artist(artist["id"])
+
     rows.append({
+        # Reproducción
         "played_at": item["played_at"],
+
+        # Track
         "track_name": track["name"],
-        "artist": track["artists"][0]["name"],
-        "album": track["album"]["name"],
+        "track_id": track["id"],
         "duration_ms": track["duration_ms"],
-        "track_id": track["id"]
+
+        # Artist
+        "artist_name": artist["name"],
+        "artist_id": artist["id"],
+        "artist_genres": ", ".join(artist_full.get("genres", [])),
+        artist_images = artist_full.get("images", [])
+        artist_img = artist_images[1]["url"] if len(artist_images) > 1 else (artist_images[0]["url"] if artist_images else None)
+
+        "artist_img": artist_img,
+
+        # Album
+        "album_name": album["name"],
+        "album_id": album["id"],
+        "album_release_year": album.get("release_date", "")[:4],
+        "album_label": album.get("label"),
+        album_images = album.get("images", [])
+        album_img = album_images[1]["url"] if len(album_images) > 1 else (album_images[0]["url"] if album_images else None)
+
+        "album_img": album_img
     })
 
 df_new = pd.DataFrame(rows)
@@ -55,3 +82,4 @@ df_total.sort_values("played_at", inplace=True)
 df_total.to_csv(DATA_PATH, index=False)
 
 print(f"Histórico actualizado: {len(df_total)} filas")
+
